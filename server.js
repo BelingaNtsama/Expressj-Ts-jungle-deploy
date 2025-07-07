aca4773bf98a37e51f99581706f3f14e88085c6c
TSweb <raymond2belinga@gmail.com>
2025-07-07 14:22:31 +0100

    Affichage d'un message.

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const { initializeWebSocket } = require('./Services/websocketService');
 // Initialisation du WebSocket
 initializeWebSocket(server);
 
+app.get("/",(req,res)=>{
+  res.send("Bienvenu sur Ts-jungle")
+})
+
 // Utilisation de server.listen au lieu de app.listen
 server.listen(PORT, () => {
   console.log(`✅ Serveur en cours d'exécution sur le port ${PORT}`);
