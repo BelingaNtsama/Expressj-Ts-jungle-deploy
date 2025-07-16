32e910ae74c6811de5201cc6316037308aaa560f
TSweb <raymond2belinga@gmail.com>
2025-07-16 11:11:30 +0100

    Correctif de securite

--- Chatbot/index.js
+++ Chatbot/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const router = express.Router();
+require('dotenv').config();
 
-const genAI = new GoogleGenerativeAI("AIzaSyATdKpQfKcrqDNBR7AgtH5pnWWJGUXJNjk");
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 router.post('/chat', async (req, res) => {
